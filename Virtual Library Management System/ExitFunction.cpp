1b57cfc6bc80b19dafba9777375718a9bbb72c4e
JeffZoe02 <165102926+JeffZoe02@users.noreply.github.com>
2024-04-28 21:37:37 -0400

    Create ExitFunction.cpp

---
+++ Virtual Library Management System/ExitFunction.cpp
@@ -0,0 +1,57 @@
+#include <iostream>  // Include for I/O stream operations
+
+using namespace std;  // Make all names under std accessible without std::
+
+void userMenu() {
+    int choice = 0;
+    while (true) {
+        cout << "\nUser Menu\n";
+        cout << "1. View Profile\n";
+        cout << "2. Edit Profile\n";
+        cout << "3. Logout\n";
+        cout << "Choose an option or 3 to exit: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Viewing profile...\n";
+                break;
+            case 2:
+                cout << "Editing profile...\n";
+                break;
+            case 3:
+                cout << "Logging out...\n";
+                return;  // Returns to the calling function, effectively exiting the menu
+            default:
+                cout << "Invalid option, try again.\n";
+        }
+    }
+}
+
+void exitApplication() {
+    cout << "Exiting application...\n";
+    exit(0);  // Exits the program
+}
+
+int main() {
+    while (true) {
+        cout << "Main Menu\n";
+        cout << "1. Login\n";
+        cout << "2. Exit\n";
+        cout << "Choose an option or 2 to exit: ";
+        int mainChoice;
+        cin >> mainChoice;
+
+        switch (mainChoice) {
+            case 1:
+                userMenu();  // Goes into the user menu
+                break;
+            case 2:
+                exitApplication();  // Call the exit function
+                break;
+            default:
+                cout << "Invalid option, try again.\n";
+        }
+    }
+    return 0;  // Not typically reached
+}
