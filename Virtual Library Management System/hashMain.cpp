25a402e0cad0a40871c0bda5240a27a13a1091e1
EtzySS <165110089+EtzySS@users.noreply.github.com>
2024-04-29 19:05:31 -0700

    Update hashMain.cpp

--- Virtual Library Management System/hashMain.cpp
+++ Virtual Library Management System/hashMain.cpp
@@ -4,7 +4,7 @@
 // Hash table Final Project
 
 #include <iostream>
-#include "user.h"
+#include "User.h"
 #include "admin.h"
 #include "hash.h"
 
@@ -12,24 +12,24 @@ int main() {
     HashTable ht;
 
     // Inserting users
-    ht.insertUser(User("David", "password1"));
-    ht.insertUser(User("Lucas", "password2"));
-    ht.insertUser(User("Alex", "password3"));
-    ht.insertUser(User("Jackson", "password4"));
-    ht.insertUser(User("Emily", "password5"));
-    ht.insertUser(User("Emma", "password6"));
-    ht.insertUser(User("Riley", "password7"));
-    ht.insertUser(User("Sam", "password8"));
-    ht.insertUser(User("Sammy", "password9"));
-    ht.insertUser(User("Christine", "password10"));
-    ht.insertUser(User("Penelope", "password11"));
-    ht.insertUser(User("Julian", "password12"));
-    ht.insertUser(User("Oakley", "password13"));
-    ht.insertUser(User("JJ", "password14"));
-    ht.insertUser(User("Fredrick", "password15"));
-    ht.insertUser(User("Marcel", "password16"));
-    ht.insertUser(User("Nami", "password17"));
-    ht.insertUser(User("Jack", "password18"));
+    ht.insertUser(User("David", "password1", "standard"));
+    ht.insertUser(User("Lucas", "password2", "standard"));
+    ht.insertUser(User("Alex", "password3", "standard"));
+    ht.insertUser(User("Jackson", "password4", "standard"));
+    ht.insertUser(User("Emily", "password5", "standard"));
+    ht.insertUser(User("Emma", "password6", "standard"));
+    ht.insertUser(User("Riley", "password7", "standard"));
+    ht.insertUser(User("Sam", "password8", "standard"));
+    ht.insertUser(User("Sammy", "password9", "standard"));
+    ht.insertUser(User("Christine", "password10", "standard"));
+    ht.insertUser(User("Penelope", "password11", "standard"));
+    ht.insertUser(User("Julian", "password12", "standard"));
+    ht.insertUser(User("Oakley", "password13", "standard"));
+    ht.insertUser(User("JJ", "password14", "standard"));
+    ht.insertUser(User("Fredrick", "password15", "standard"));
+    ht.insertUser(User("Marcel", "password16", "standard"));
+    ht.insertUser(User("Nami", "password17", "standard"));
+    ht.insertUser(User("Jack", "password18", "standard"));
 
   // Printing hash table
       ht.printTable();
