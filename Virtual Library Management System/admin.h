fe3a3b2fc604c64ea5cdff73428583ffc4792216
EtzySS <165110089+EtzySS@users.noreply.github.com>
2024-05-01 20:27:55 -0700

    Add files via upload
    compiled to user.h and User.cpp

---
+++ Virtual Library Management System/admin.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "user.h"
+
+class Admin : public User {
+public:
+    Admin(const std::string& uname, const std::string& pwd);
+};
+
+#endif 
