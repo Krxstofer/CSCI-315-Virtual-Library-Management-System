a9dea0a1ae92f28b0f90f432379af08d6de63335
JeffZoe02 <165102926+JeffZoe02@users.noreply.github.com>
2024-05-02 16:29:01 -0400

    Create ExitFunction.h

---
+++ Virtual Library Management System/ExitFunction.h
@@ -0,0 +1,9 @@
+#ifndef EXIT_FUNCTION_H
+#define EXIT_FUNCTION_H
+
+#include <cstdlib>  // Include for exit()
+
+// Function to exit the application cleanly
+void exitApplication();
+
+#endif // EXIT_FUNCTION_H
