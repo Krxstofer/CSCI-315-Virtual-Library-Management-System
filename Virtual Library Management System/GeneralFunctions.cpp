d47a6367d1577f78b06d3016188dabbdee196e69
JeffZoe02 <165102926+JeffZoe02@users.noreply.github.com>
2024-05-03 19:13:03 -0400

    Update GeneralFunctions.cpp

--- Virtual Library Management System/GeneralFunctions.cpp
+++ Virtual Library Management System/GeneralFunctions.cpp
@@ -150,28 +150,28 @@ bool registerUser(HashTable& hash, User& user, istream& in_stream, bool display)
 
 }//end of registerUser
 
-
-// Implementation of the login function
-
-
-// Assuming "Login.h" declares the login function and includes "hash.h" and "user.h"
-// Login function now returns the role of the user or an empty string if login fails
-std::string login(HashTable& ht, const std::string& username, const std::string& password) {
+//Login Function
+// Adjust the login function to include the verbose parameter to control output
+std::string login(HashTable& ht, const std::string& username, const std::string& password, bool verbose) {
     if (ht.checkPassword(username, password)) {
         std::string role = ht.getUserRole(username);
         if (!role.empty()) {
-            std::cout << "Login successful! Welcome, " << username << ". You are logged in as a(n) " << role << "." << std::endl;
+            if (verbose) {
+                std::cout << "Login successful! Welcome, " << username << ". You are logged in as a(n) " << role << "." << std::endl;
+            }
         } else {
-            std::cout << "User role not found." << std::endl;
+            if (verbose) {
+                std::cout << "User role not found." << std::endl;
+            }
         }
         return role;
-
     } else {
-        std::cout << "Login failed: Invalid username or password." << std::endl;
+        if (verbose) {
+            std::cout << "Login failed: Invalid username or password." << std::endl;
+        }
         return "";  // Return an empty string if authentication fails
     }
 }
-
 //Exit Function
 /*using namespace std;
 
