00410e8e3c3cfddc9509073e04c678332477cedd
andr-pal <andrpal@mail.regent.edu>
2024-05-03 18:07:35 -0500

    Moved my test to UserTest.cpp and updated hash check in UserFun.cpp

--- Virtual Library Management System/UserTests.cpp
+++ Virtual Library Management System/UserTests.cpp
@@ -13,3 +13,37 @@
 #include <gtest/gtest.h>
 
 #include <stdexcept>  // Include for std::runtime_error
+
+using namespace std;
+
+TEST(LibraryTest, updateProfile)
+{
+    HashTable hash;
+    User myUser("Ph1n3as", "S3cr3t", "User");
+
+    istringstream testUsername("1\nW1nn1f3r\n");
+    istringstream testPassword("2\nM3@oo6\nM3@oo6\n");
+    istringstream testCancel("3\n");
+
+    //Checking data before updating
+    EXPECT_EQ("Ph1n3as", myUser.getUsername());
+    EXPECT_EQ("S3cr3t", myUser.getPassword());
+
+    updateProfile(hash, myUser, testUsername, false);
+
+    //Checking data after updating username
+    EXPECT_EQ("W1nn1f3r", myUser.getUsername());
+    EXPECT_EQ("S3cr3t", myUser.getPassword());
+
+    updateProfile(hash, myUser, testPassword, false);
+
+    //Checking data after updating password
+    EXPECT_EQ("W1nn1f3r", myUser.getUsername());
+    EXPECT_EQ("M3@oo6", myUser.getPassword());
+
+    updateProfile(hash, myUser, testCancel, false);
+
+    //Checking data after cancling
+    EXPECT_EQ("W1nn1f3r", myUser.getUsername());
+    EXPECT_EQ("M3@oo6", myUser.getPassword());
+}
